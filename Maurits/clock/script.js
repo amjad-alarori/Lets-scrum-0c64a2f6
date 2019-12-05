f040f5a0e9b7ce30a1c8e0e746f769766246fbc2
MAURITS DOES <2111633@talnet.nl>
2019-12-05 11:30:39 +0100

    focus timer compleet

--- Maurits/clock/script.js
+++ Maurits/clock/script.js
@@ -1,11 +1,8 @@
 var target = document.getElementById("target");
 var time = document.getElementById("time");
-function countdown(target, time) {
-    time = time.value;
-    var split_time = time.split(":");
-    var uren = Number(split_time[0]);
-    var minuten = Number(split_time[1]);
-    var seconds = uren * 3600 + minuten * 60;
+var seconds;
+var countdownTimer;
+function countdown() {
 
     var days = Math.floor(seconds / 24 / 60 / 60);
 
@@ -19,18 +16,21 @@ function countdown(target, time) {
     function pad(n) {
         return (n < 10 ? "0" + n : n);
     }
-    document.getElementById("target"). innerHTML = pad(hours) + ":" + pad(minutes) + ":" + pad(remainingSeconds);
+    target. innerHTML = pad(hours) + ":" + pad(minutes) + ":" + pad(remainingSeconds);
     if (seconds === 0) {
         clearInterval(countdownTimer);
         document.getElementById("target"). innerHTML = "Completed!"
     } else {
-        seconds--;
+        seconds --;
     }
 }
-var countdownTimer = setInterval("countdown()", 1000);
+function run() {
+    time = time.value;
+    var split_time = time.split(":");
+    var uren = Number(split_time[0]);
+    var minuten = Number(split_time[1]);
+    seconds = uren * 3600 + minuten * 60;
 
-// var hoursLeft   = Math.floor((seconds) - (days*86400));
-// var hours       = Math.floor(hoursLeft/3600);
-// var minutesLeft = Math.floor((hoursLeft) - (hours*3600));
-// var minutes     = Math.floor(minutesLeft/60);
-// var remainingSeconds = seconds % 60;
+    countdownTimer = setInterval("countdown()", 1000);
+}
+
